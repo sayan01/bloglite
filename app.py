cacda663de67f9d8c96eaa4ef1ef15a023246e34
Sayan Ghosh <sgic2001@gmail.com>
2023-01-07 12:34:18 +0530

    Add error pages

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ def get_user(id):
     user = {"username": "sayan", "fname": "Sayan", "lname": "Ghosh", "about": "I code stuff", "id": id})
 
 
+# error pages
+# invalid url
+@app.errorhandler(404)
+def error404(e):
+    return render_template("error/404.html", error=e), 404
+
 
 
 if __name__ == '__main__':
